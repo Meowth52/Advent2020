bcea685e9095bb646227ca2ed60b79a13d5a5dc7
Meowth52 <meowth@meowth.se>
2020-12-01 08:01:48 +0100

    Day 1

--- Day01.cs
+++ Day01.cs
@@ -9,9 +9,10 @@ namespace Advent2020
 {
     public class Day01 : Day
     {
+        List<int> Instructions;
         public Day01(string _input) : base(_input)
         {
-
+            Instructions = this.parseListOfInteger(_input);
         }
         public override Tuple<string, string> getResult()
         {
@@ -20,11 +21,24 @@ namespace Advent2020
         public string getPartOne()
         {
             int ReturnValue = 0;
+            foreach (int i in Instructions)
+            {
+                foreach (int i2 in Instructions)
+                {
+                    if (i + i2 == 2020)
+                        ReturnValue = i * i2;
+                }
+            }
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
             int ReturnValue = 0;
+            foreach (int i in Instructions)
+                foreach (int i2 in Instructions)
+                    foreach (int i3 in Instructions)
+                        if (i + i2 +i3 == 2020)
+                            ReturnValue = i * i2 *i3;
             return ReturnValue.ToString();
         }
     }
