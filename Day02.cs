9920a5ed22994346a81fbc9a40b705b1cb989a13
Meowth52 <meowth@meowth.se>
2020-12-02 08:30:40 +0100

    Day 2

--- Day02.cs
+++ Day02.cs
@@ -9,9 +9,10 @@ namespace Advent2020
 {
     public class Day02 : Day
     {
+        string[] Instructions;
         public Day02(string _input) : base(_input)
         {
-
+            Instructions = this.parseStringArray(_input);
         }
         public override Tuple<string, string> getResult()
         {
@@ -20,11 +21,34 @@ namespace Advent2020
         public string getPartOne()
         {
             int ReturnValue = 0;
+            foreach (string s in Instructions)
+            {
+                MatchCollection Matches = new Regex("\\d+").Matches(s);
+                int Min = Int32.Parse(Matches[0].Value);
+                int Max = Int32.Parse(Matches[1].Value);
+                Matches = new Regex("[a-z]+").Matches(s);
+                string c = Matches[0].Value;
+                string Password = Matches[1].Value;
+                int PasswordMatches = (Password.Length - Password.Replace(c, "").Length);
+                if (PasswordMatches >= Min && PasswordMatches <= Max)
+                    ReturnValue++;
+            }
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
             int ReturnValue = 0;
+            foreach (string s in Instructions)
+            {
+                MatchCollection Matches = new Regex("\\d+").Matches(s);
+                int Min = Int32.Parse(Matches[0].Value);
+                int Max = Int32.Parse(Matches[1].Value);
+                Matches = new Regex("[a-z]+").Matches(s);
+                char c = Matches[0].Value[0];
+                string Password = Matches[1].Value;
+                if (Password[Min - 1] == c ^ Password[Max - 1] == c)
+                    ReturnValue++;
+            }
             return ReturnValue.ToString();
         }
     }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Advent2020
 {
     public partial class MainWindow : Window
     {
-        int LastDay = 1;
+        int LastDay = 2;
         public int ChoosenDay;
         private readonly MainView _mainView;
         public MainWindow()
