d06f9845d8e3a2f341e8d9f4e30f9da4469cae43
Meowth52 <meowth@meowth.se>
2020-12-04 08:19:41 +0100

    Day 4 part one done part two not really

--- Day04.cs
+++ Day04.cs
@@ -9,9 +9,11 @@ namespace Advent2020
 {
     public class Day04 : Day
     {
+        string[] Instructions;
         public Day04(string _input) : base(_input)
         {
-
+            string cleaned = _input.Replace("\r\n\r\n", "_").Replace("\r\n", "=").Replace(" ", "=");
+            Instructions = this.parseStringArray(cleaned);
         }
         public override Tuple<string, string> getResult()
         {
@@ -20,11 +22,82 @@ namespace Advent2020
         public string getPartOne()
         {
             int ReturnValue = 0;
+            string[] Pass = { "byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:" };
+            foreach (string s in Instructions)
+            {
+                bool mhm = true;
+                foreach (string ss in Pass)
+                {
+                    if (!s.Contains(ss))
+                        mhm = false;
+                }
+                if (mhm)
+                    ReturnValue++;
+            }
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
             int ReturnValue = 0;
+            foreach (string s in Instructions)
+            {
+                int Validness = 0;
+                string[] Document = s.Split('=');
+                foreach (string ss in Document)
+                {
+                    string[] DicItem = ss.Split(':');
+                    int Number = 0;
+                    switch (DicItem[0])
+                    {
+                        case "byr":
+                            Int32.TryParse(DicItem[1], out Number);
+                            if (Number >= 1920 && Number <= 2002)
+                                Validness++;
+                            break;
+                        case "iyr":
+                            Int32.TryParse(DicItem[1], out Number);
+                            if (Number >= 2010 && Number <= 2020)
+                                Validness++;
+                            break;
+                        case "eyr":
+                            Int32.TryParse(DicItem[1], out Number);
+                            if (Number >= 2020 && Number <= 2030)
+                                Validness++;
+                            break;
+                        case "hgt":
+                            if (DicItem[1].Contains("cm"))
+                            {
+                                Int32.TryParse(DicItem[1].Replace("cm", ""), out Number);
+                                if (Number >= 150 && Number <= 193)
+                                    Validness++;
+                            }
+                            if (DicItem[1].Contains("in")) //Should we really let these guy in?
+                            {
+                                Int32.TryParse(DicItem[1].Replace("in", ""), out Number);
+                                if (Number >= 59 && Number <= 76)
+                                    Validness++;
+                            }
+                            break;
+                        case "hcl":
+                            Int32.TryParse(DicItem[1].Replace("#", ""), System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture, out Number);
+                            if (Number != )
+                                break;
+                        case "ecl":
+                            ;
+                            break;
+                        case "pid":
+                            ;
+                            break;
+                        case "cid":
+                            ; //sssh
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                if (Validness >= 7)
+                    ReturnValue++;
+            }
             return ReturnValue.ToString();
         }
     }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Advent2020
 {
     public partial class MainWindow : Window
     {
-        int LastDay = 2;
+        int LastDay = 4;
         public int ChoosenDay;
         private readonly MainView _mainView;
         public MainWindow()
