4eb2bc0f0c0d0623ae82b604e7d37bc1fe1b3636
Meowth52 <meowth@meowth.se>
2020-12-06 11:45:11 +0100

    Day 6

--- Day06.cs
+++ Day06.cs
@@ -9,9 +9,11 @@ namespace Advent2020
 {
     public class Day06 : Day
     {
+        string[] Instructions;
         public Day06(string _input) : base(_input)
         {
-
+            string clean = _input.Replace("\r\n\r\n", "_").Replace("\r\n", ",");
+            Instructions = this.parseStringArray(clean);
         }
         public override Tuple<string, string> getResult()
         {
@@ -20,11 +22,43 @@ namespace Advent2020
         public string getPartOne()
         {
             int ReturnValue = 0;
+            foreach (string s in Instructions)
+            {
+                List<char> Answeres = new List<char>();
+                foreach (char c in s)
+                {
+                    if (c != ',' && !Answeres.Contains(c))
+                        Answeres.Add(c);
+                }
+                ReturnValue += Answeres.Count;
+            }
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
             int ReturnValue = 0;
+            List<string[]> Passengers = new List<string[]>();
+            foreach (string s in Instructions)
+            {
+                Passengers.Add(s.Split(','));
+            }
+            foreach (string[] g in Passengers)
+            {
+                Dictionary<char, int> Answeres = new Dictionary<char, int>();
+                foreach (string s in g)
+                {
+                    foreach (char c in s)
+                    {
+                        if (Answeres.ContainsKey(c))
+                            Answeres[c]++;
+                        else
+                            Answeres.Add(c, 1);
+                    }
+                }
+                foreach (KeyValuePair<char, int> k in Answeres)
+                    if (k.Value == g.Length)
+                        ReturnValue++;
+            }
             return ReturnValue.ToString();
         }
     }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Advent2020
 {
     public partial class MainWindow : Window
     {
-        int LastDay = 5;
+        int LastDay = 6;
         public int ChoosenDay;
         private readonly MainView _mainView;
         public MainWindow()
