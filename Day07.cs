8b31dd90b09ebc3e32e7499d112a3ff00ef788f9
Meowth52 <meowth@meowth.se>
2020-12-07 08:28:32 +0100

    Day 7 parsing

--- Day07.cs
+++ Day07.cs
@@ -9,9 +9,10 @@ namespace Advent2020
 {
     public class Day07 : Day
     {
+        string[] Instructions;
         public Day07(string _input) : base(_input)
         {
-
+            Instructions = this.parseStringArray(_input);
         }
         public override Tuple<string, string> getResult()
         {
@@ -20,6 +21,18 @@ namespace Advent2020
         public string getPartOne()
         {
             int ReturnValue = 0;
+            Dictionary<string, Dictionary<string, int>> Bags = new Dictionary<string, Dictionary<string, int>>();
+            foreach (string s in Instructions)
+            {
+                string[] wordSplit = s.Split(' ');
+                string Bag = wordSplit[0] + wordSplit[1];
+                Bags.Add(Bag, new Dictionary<string, int>());
+                for (int i = 7; i < wordSplit.Length; i++)
+                {
+                    if (wordSplit[i].Contains("bag"))
+                        Bags[Bag].Add(wordSplit[i - 2] + wordSplit[i - 1], Int32.Parse(wordSplit[i - 3]));
+                }
+            }
             return ReturnValue.ToString();
         }
         public string getPartTwo()
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Advent2020
 {
     public partial class MainWindow : Window
     {
-        int LastDay = 6;
+        int LastDay = 7;
         public int ChoosenDay;
         private readonly MainView _mainView;
         public MainWindow()
