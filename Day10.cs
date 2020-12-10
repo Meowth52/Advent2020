7fc87d5911626a001d15dd2db06e43619691cf87
Meowth52 <meowth@meowth.se>
2020-12-10 08:29:52 +0100

    day 10 part 1

--- Day10.cs
+++ Day10.cs
@@ -9,9 +9,11 @@ namespace Advent2020
 {
     public class Day10 : Day
     {
+        List<int> Instructions;
         public Day10(string _input) : base(_input)
         {
-
+            Instructions = this.parseListOfInteger(_input);
+            Instructions.Sort();
         }
         public override Tuple<string, string> getResult()
         {
@@ -20,11 +22,35 @@ namespace Advent2020
         public string getPartOne()
         {
             int ReturnValue = 0;
+            int Jolt1 = 0;
+            int Jolt3 = 0;
+            int Last = 0;
+            foreach (int i in Instructions)
+            {
+                if (i - Last == 1)
+                    Jolt1++;
+                if (i - Last == 3)
+                    Jolt3++;
+                Last = i;
+            }
+            Jolt3++;
+            ReturnValue = Jolt1 * Jolt3;
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
             int ReturnValue = 0;
+            int Last1 = -20;
+            int last2 = -10;
+            int last3 = 0;
+            foreach (int i in Instructions)
+            {
+                if (i - Last1 <= 3)
+                    ReturnValue++;
+                Last1 = last2;
+                last2 = last3;
+                last3 = i;
+            }
             return ReturnValue.ToString();
         }
     }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Advent2020
 {
     public partial class MainWindow : Window
     {
-        int LastDay = 9;
+        int LastDay = 10;
         public int ChoosenDay;
         private readonly MainView _mainView;
         public MainWindow()
