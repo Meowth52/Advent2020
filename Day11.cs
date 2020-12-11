6f356b04c3d3dd197fb7518a26331ac124e2afa4
Meowth52 <meowth@meowth.se>
2020-12-11 08:39:37 +0100

    Day 11 not done

--- Day11.cs
+++ Day11.cs
@@ -9,9 +9,20 @@ namespace Advent2020
 {
     public class Day11 : Day
     {
+        string[] Instructions;
+        Dictionary<string, ChairMaybe> Area;
+
         public Day11(string _input) : base(_input)
         {
-
+            Instructions = this.parseStringArray(_input);
+            Area = new Dictionary<string, ChairMaybe>();
+            for (int y = 0; y < Instructions.Count(); y++)
+            {
+                for (int x = 0; x < Instructions[y].Length; x++)
+                {
+                    Area.Add(x.ToString() + y.ToString(), new ChairMaybe(x, y, Instructions[y][x] == 'L'));
+                }
+            }
         }
         public override Tuple<string, string> getResult()
         {
@@ -20,12 +31,44 @@ namespace Advent2020
         public string getPartOne()
         {
             int ReturnValue = 0;
+            while (true)
+            {
+                Dictionary<string, ChairMaybe> NextState = new Dictionary<string, ChairMaybe>(Area);
+                foreach (KeyValuePair<string, ChairMaybe> c in Area)
+                {
+                    foreach (string s in c.ge)
+                }
+            }
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
             int ReturnValue = 0;
             return ReturnValue.ToString();
+        }
+    }
+    public class ChairMaybe : Coordinate
+    {
+        public bool IsChair;
+        public bool IsOccupied;
+        int[,] AllDirections = new int[8, 2] { { 1, -1 }, { 0, 1 }, { 1, 0 }, { 1, 1 }, { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 } };
+        public ChairMaybe(int x, int y, bool isChair) : base(x, y)
+        {
+            this.x = x;
+            this.y = y;
+            IsChair = isChair;
+            IsOccupied = false;
+        }
+        public List<string> GetAllNeighbourKeys() //Use hashcode instead?
+        {
+            List<string> ReturnList = new List<string>();
+            for (int i = 0; i < 8; i++)
+            {
+                int _x = x + AllDirections[i, 0];
+                int _y = y + AllDirections[i, 1];
+                ReturnList.Add(x.ToString() + y.ToString());
+            }
+            return ReturnList;
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Advent2020
 {
     public partial class MainWindow : Window
     {
-        int LastDay = 10;
+        int LastDay = 11;
         public int ChoosenDay;
         private readonly MainView _mainView;
         public MainWindow()
