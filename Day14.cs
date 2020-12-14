f0bba937a696e1a4d16ee04965f3c169382be042
Meowth52 <meowth@meowth.se>
2020-12-14 22:24:06 +0100

    uuuuuu

--- Day14.cs
+++ Day14.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Numerics;
 
 namespace Advent2020
 {
     public class Day14 : Day
     {
         string[] Instructions;
-        Dictionary<int, long> Memory;
+        Dictionary<ulong, ulong> Memory;
         List<DataSet> Sets = new List<DataSet>();
 
         public Day14(string _input) : base(_input)
         {
             Instructions = this.parseStringArray(_input.Replace("\r\nmem", "mem"));
-            Memory = new Dictionary<int, long>();
+            Memory = new Dictionary<ulong, ulong>();
         }
         public override Tuple<string, string> getResult()
         {
@@ -21,7 +22,7 @@ namespace Advent2020
         }
         public string getPartOne()
         {
-            long ReturnValue = 0;
+            ulong ReturnValue = 0;
             foreach (string s in Instructions)
             {
                 Sets.Add(new DataSet(s));
@@ -30,7 +31,7 @@ namespace Advent2020
             {
                 s.MemoryWrite(ref Memory);
             }
-            foreach (KeyValuePair<int, long> m in Memory)
+            foreach (KeyValuePair<ulong, ulong> m in Memory)
             {
                 ReturnValue += m.Value;
             }
@@ -38,10 +39,15 @@ namespace Advent2020
         }
         public string getPartTwo()
         {
-            long ReturnValue = 0;
+            BigInteger ReturnValue = 0;
+            Memory = new Dictionary<ulong, ulong>();
             foreach (DataSet s in Sets)
             {
-                ReturnValue += s.FLoatingSum();
+                s.FLoatingSum(ref Memory);
+            }
+            foreach (KeyValuePair<ulong, ulong> m in Memory)
+            {
+                ReturnValue = BigInteger.Add(ReturnValue, m.Value);
             }
             return ReturnValue.ToString();
         }
@@ -49,11 +55,11 @@ namespace Advent2020
     public class DataSet
     {
         string Mask;
-        List<Tuple<int, int>> Values;
+        List<Tuple<ulong, ulong>> Values;
         public DataSet(string _input)
         {
             string[] input = _input.Split(new[] { "mem" }, StringSplitOptions.RemoveEmptyEntries);
-            Values = new List<Tuple<int, int>>();
+            Values = new List<Tuple<ulong, ulong>>();
             foreach (string s in input)
             {
                 if (s.Contains("mask"))
@@ -63,15 +69,15 @@ namespace Advent2020
                 else
                 {
                     MatchCollection Matches = Regex.Matches(s, @"-?\d+");
-                    Values.Add(new Tuple<int, int>(Int32.Parse(Matches[0].Value), Int32.Parse(Matches[1].Value)));
+                    Values.Add(new Tuple<ulong, ulong>(ulong.Parse(Matches[0].Value), ulong.Parse(Matches[1].Value)));
                 }
             }
         }
-        public void MemoryWrite(ref Dictionary<int, long> Memory)
+        public void MemoryWrite(ref Dictionary<ulong, ulong> Memory)
         {
-            foreach (Tuple<int, int> t in Values)
+            foreach (Tuple<ulong, ulong> t in Values)
             {
-                string ValueString = Convert.ToString(t.Item2, 2);
+                string ValueString = Convert.ToString((long)t.Item2, 2);
                 ValueString = ValueString.PadLeft(36, '0');
                 string Maskedvalue = "";
                 for (int i = 0; i < Mask.Length; i++)
@@ -93,22 +99,22 @@ namespace Advent2020
                             break;
                     }
                 }
-                long masked = Convert.ToInt64(Maskedvalue, 2);
+                ulong masked = Convert.ToUInt64(Maskedvalue, 2);
                 if (Memory.ContainsKey(t.Item1))
                     Memory[t.Item1] = masked;
                 else
                     Memory.Add(t.Item1, masked);
             }
         }
-        public FLoatingSum()
+        public void FLoatingSum(ref Dictionary<ulong, ulong> Memory)
         {
-            long ReturnValue = 0;
-            foreach (Tuple<int, int> t in Values)
+            foreach (Tuple<ulong, ulong> t in Values)
             {
-                string ValueString = Convert.ToString(t.Item2, 2);
+                string ValueString = Convert.ToString((long)t.Item1, 2);
                 ValueString = ValueString.PadLeft(36, '0');
-                double Maskedvalue = 0;
-                int HighestValue = 0;
+                List<ulong> Adresses = new List<ulong>();
+                Adresses.Add(0);
+                ulong Solid = 0;
                 for (int i = 0; i < Mask.Length; i++)
                 {
                     int inv = Mask.Length - (i + 1);
@@ -119,20 +125,29 @@ namespace Advent2020
                             break;
                         case "0X":
                         case "1X":
-                            Maskedvalue += Math.Pow(2, i) / 2;
-                            HighestValue++;
+                            int count = Adresses.Count;
+                            for (int a = 0; a < count; a++)
+                            {
+                                Adresses.Add((ulong)a);
+                                Adresses[a] += (ulong)Math.Pow(2, i);
+                            }
                             break;
                         case "11":
                         case "01":
-                            Maskedvalue += Math.Pow(2, i);
+                        case "10":
+                            Solid += (ulong)Math.Pow(2, i);
                             break;
                         default:
                             break;
                     }
                 }
-                ReturnValue += (long)Maskedvalue * (int)Math.Pow(2, HighestValue);
+                foreach (ulong l in Adresses)
+                {
+                    Memory[l + Solid] = t.Item2;
+                }
+                ;
             }
-            return ReturnValue;
+            ;
         }
     }
 
