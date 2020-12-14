d78b5f64214e58ed4606828398e73b8d2948d252
Meowth <NoThisIsAPublicRepo@No.No>
2020-12-14 16:47:46 +0100

    Not done

--- Day14.cs
+++ Day14.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
 namespace Advent2020
 {
     public class Day14 : Day
     {
+        string[] Instructions;
+        Dictionary<int, int> Memory;
+
         public Day14(string _input) : base(_input)
         {
-
+            Instructions = this.parseStringArray(_input.Replace("\r\nmem", "mem"));
+            Memory = new Dictionary<int, int>();
         }
         public override Tuple<string, string> getResult()
         {
@@ -20,6 +21,11 @@ namespace Advent2020
         public string getPartOne()
         {
             int ReturnValue = 0;
+            List<DataSet> Sets = new List<DataSet>();
+            foreach (string s in Instructions)
+            {
+                Sets.Add(new DataSet(s));
+            }
             return ReturnValue.ToString();
         }
         public string getPartTwo()
@@ -28,4 +34,44 @@ namespace Advent2020
             return ReturnValue.ToString();
         }
     }
+    public class DataSet
+    {
+        string Mask;
+        List<Tuple<int, int>> Values;
+        public DataSet(string _input)
+        {
+            string[] input = _input.Split(new[] { "mem" }, StringSplitOptions.RemoveEmptyEntries);
+            Values = new List<Tuple<int, int>>();
+            foreach (string s in input)
+            {
+                if (s.Contains("mask"))
+                {
+                    Mask = Regex.Match(s, @"[10X]+").Value;
+                }
+                else
+                {
+                    MatchCollection Matches = Regex.Matches(s, @"-?\d+");
+                    Values.Add(new Tuple<int, int>(Int32.Parse(Matches[0].Value), Int32.Parse(Matches[1].Value)));
+                }
+            }
+            ;
+        }
+        public void MemoryWrite(ref Dictionary<int, int> Memory)
+        {
+            foreach (Tuple<int, int> t in Values)
+            {
+                int masked = 0;
+                string ValueString = Convert.ToString(t.Item2, 2);
+                for (int i = 0; i < Mask.Length; i++)
+                {
+
+                }
+                if (Memory.ContainsKey(t.Item1))
+                    Memory[t.Item1] = masked;
+                else
+                    Memory.Add(t.Item1, masked);
+            }
+        }
+    }
+
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Diagnostics;
 
 namespace Advent2020
 {
     public partial class MainWindow : Window
     {
-        int LastDay = 13;
+        int LastDay = 14;
         public int ChoosenDay;
         private readonly MainView _mainView;
         public MainWindow()
