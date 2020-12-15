d69c29b98403a3480a554601f729be353cc2fc96
Meowth <NoThisIsAPublicRepo@No.No>
2020-12-15 16:59:07 +0100

    nopnope

--- Day15.cs
+++ Day15.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
 namespace Advent2020
 {
     public class Day15 : Day
     {
+        List<int> Instructions;
         public Day15(string _input) : base(_input)
         {
-
+            Instructions = this.parseListOfInteger(_input);
         }
         public override Tuple<string, string> getResult()
         {
@@ -20,6 +17,34 @@ namespace Advent2020
         public string getPartOne()
         {
             int ReturnValue = 0;
+            Dictionary<int, int> Spoken = new Dictionary<int, int>();
+            int Turn = 1;
+            int Last = 0;
+            foreach (int i in Instructions)
+            {
+                Spoken.Add(i, 0);
+                Last = i;
+                Turn++;
+            }
+            ;
+            while (Turn <= 2020)
+            {
+                if (Spoken.ContainsKey(Last) && Spoken[Last] > 0)
+                {
+                    ReturnValue = Turn - Spoken[Last];
+                    Spoken[Last] = Turn;
+                }
+                else if (!Spoken.ContainsKey(Last))
+                {
+                    Spoken[Last] = 0;
+                    ReturnValue = 0;
+                }
+                else
+                    Spoken[Last] = Turn;
+                Last = ReturnValue;
+                Turn++;
+            }
+            ReturnValue = Last;
             return ReturnValue.ToString();
         }
         public string getPartTwo()
