42cbb49118e292fac7f4dae3abd753da7d555460
Meowth52 <meowth@meowth.se>
2020-12-19 14:26:50 +0100

    Day 19 basic parsing

--- Day19.cs
+++ Day19.cs
@@ -9,9 +9,18 @@ namespace Advent2020
 {
     public class Day19 : Day
     {
+        Dictionary<int, RuleSet> RuleSets;
+        string[] Messages;
         public Day19(string _input) : base(_input)
         {
-
+            string[] Inputs = _input.Replace("\r\n\r\n", "_").Split('_');
+            RuleSets = new Dictionary<int, RuleSet>();
+            foreach (string s in this.parseStringArray(Inputs[0]))
+            {
+                string[] Splitted = s.Split(':');
+                RuleSets.Add(Int32.Parse(Splitted[0]), new RuleSet(Splitted[1]));
+            }
+            Messages = this.parseStringArray(Inputs[1]);
         }
         public override Tuple<string, string> getResult()
         {
@@ -20,12 +29,35 @@ namespace Advent2020
         public string getPartOne()
         {
             int ReturnValue = 0;
+            foreach (string m in Messages)
+            {
+                string Message = m;
+                int Index = 0;
+                while (true)
+                {
+                    //Actually solve the day
+                }
+            }
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
             int ReturnValue = 0;
             return ReturnValue.ToString();
+        }
+        public class RuleSet
+        {
+            List<List<string>> Rules;
+            public RuleSet(string input)
+            {
+                Rules = new List<List<string>>();
+                string[] Splitted = input.Split('|');
+                foreach (string s in Splitted)
+                {
+                    string[] Schplitted = s.Replace("\"", "").Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    Rules.Add(Schplitted.ToList());
+                }
+            }
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,7 +8,7 @@ namespace Advent2020
 {
     public partial class MainWindow : Window
     {
-        int LastDay = 18;
+        int LastDay = 19;
         public int ChoosenDay;
         private readonly MainView _mainView;
         public MainWindow()
