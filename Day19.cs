17abe15bafeb70d15ff3db3e94f12e9be0460eb2
Meowth52 <meowth@meowth.se>
2020-12-20 14:03:34 +0100

    day 19 part 1

--- Day19.cs
+++ Day19.cs
@@ -11,6 +11,7 @@ namespace Advent2020
     {
         Dictionary<int, RuleSet> RuleSets;
         string[] Messages;
+        List<string> ValidMessages;
         public Day19(string _input) : base(_input)
         {
             string[] Inputs = _input.Replace("\r\n\r\n", "_").Split('_');
@@ -21,6 +22,7 @@ namespace Advent2020
                 RuleSets.Add(Int32.Parse(Splitted[0]), new RuleSet(Splitted[1]));
             }
             Messages = this.parseStringArray(Inputs[1]);
+            ValidMessages = RuleSets[0].PossibleStrings(ref RuleSets, 0);
         }
         public override Tuple<string, string> getResult()
         {
@@ -31,18 +33,24 @@ namespace Advent2020
             int ReturnValue = 0;
             foreach (string m in Messages)
             {
-                string Message = m;
-                int Index = 0;
-                while (true)
-                {
-                    //Actually solve the day
-                }
+                if (ValidMessages.Contains(m))
+                    ReturnValue++;
             }
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
             int ReturnValue = 0;
+            //RuleSets[8] = new RuleSet("42 | 42 8");
+            //RuleSets[11] = new RuleSet("42 31 | 42 11 31");
+            //RuleSets[8] = new RuleSet("42 | 42 42| 42 42 42| 42 42 42 42 | 42 42 42 42 42 | 42 42 42 42 42 42 | 42 42 42 42 42 42 42 | 42 42 42 42 42 42 42 42 | 42 42 42 42 42 42 42 42 42 | 42 42 42 42 42 42 42 42 42 42");
+            //RuleSets[11] = new RuleSet("42 31 | 42 42 31 31 | 42 42 42 31 31 31 | 42 42 42 42 31 31 31 31  | 42 42 42 42 42 31 31 31 31 31 | 42 42 42 42 42 42 31 31 31 31 31 31 | 42 42 42 42 42 42 42 31 31 31 31 31 31 31 | 42 42 42 42 42 42 42 42 31 31 31 31 31 31 31 31 | 42 42 42 42 42 42 42 42 42 31 31 31 31 31 31 31 31 31 | 42 42 42 42 42 42 42 42 42 42 31 31 31 31 31 31 31 31 31 31");
+            //ValidMessages = RuleSets[0].PossibleStrings(ref RuleSets, 0);
+            //foreach (string m in Messages)
+            //{
+            //    if (ValidMessages.Contains(m))
+            //        ReturnValue++;
+            //}
             return ReturnValue.ToString();
         }
         public class RuleSet
@@ -57,6 +65,41 @@ namespace Advent2020
                     string[] Schplitted = s.Replace("\"", "").Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                     Rules.Add(Schplitted.ToList());
                 }
+            }
+            public List<string> PossibleStrings(ref Dictionary<int, RuleSet> RuleSets, int Depth)
+            {
+                Depth++;
+                List<string> ReturnList = new List<string>();
+                if (Depth > 15)
+                    return ReturnList;
+                if (Rules[0][0] == "a" || Rules[0][0] == "b")
+                {
+                    ReturnList.Add(Rules[0][0]);
+                    return ReturnList;
+                }
+                foreach (List<string> l in Rules)
+                {
+                    HashSet<string> Hashis = new HashSet<string>();
+                    foreach (string s in l)
+                    {
+                        List<string> Childs = RuleSets[Int32.Parse(s)].PossibleStrings(ref RuleSets, Depth);
+                        if (Hashis.Count == 0)
+                            foreach (string c in Childs)
+                                Hashis.Add(c);
+                        else
+                        {
+                            HashSet<string> Hashiser = new HashSet<string>();
+                            foreach (string a in Hashis)
+                                foreach (string c in Childs)
+                                    if (a.Length + c.Length < 98)
+                                        Hashiser.Add(a + c);
+                            Hashis = new HashSet<string>(Hashiser);
+                        }
+
+                    }
+                    ReturnList.AddRange(Hashis);
+                }
+                return ReturnList;
             }
         }
     }
