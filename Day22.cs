0329cb10519c915a3f6364ca97739c028510db8d
Meowth52 <meowth@meowth.se>
2020-12-29 13:50:32 +0100

    Day 22

--- Day22.cs
+++ Day22.cs
@@ -12,10 +12,13 @@ namespace Advent2020
         List<string[]> Instructions;
         Queue<int> Deck1;
         Queue<int> Deck2;
+        Queue<int> RDeck1;
+        Queue<int> RDeck2;
 
         public Day22(string _input) : base(_input)
         {
-            Instructions = this.parseListOfStringArrays(_input);
+            string Input = this.CheckFile(_input);
+            Instructions = this.parseListOfStringArrays(Input);
             Deck1 = new Queue<int>();
             foreach (string s in Instructions[0])
                 if (!s.Contains("Player"))
@@ -24,6 +27,8 @@ namespace Advent2020
             foreach (string s in Instructions[1])
                 if (!s.Contains("Player"))
                     Deck2.Enqueue(Int32.Parse(s));
+            RDeck1 = new Queue<int>(Deck1);
+            RDeck2 = new Queue<int>(Deck2);
         }
         public override Tuple<string, string> getResult()
         {
@@ -64,7 +69,46 @@ namespace Advent2020
         public string getPartTwo()
         {
             int ReturnValue = 0;
+            Crab(RDeck1, RDeck2);
+            ReturnValue = CountScore(RDeck1.Count > 0 ? RDeck1 : RDeck2);
             return ReturnValue.ToString();
+        }
+        public bool Crab(Queue<int> Deck1, Queue<int> Deck2)
+        {
+            int ReturnValue = 0;
+            HashSet<string> AllTheRounds = new HashSet<string>();
+            while (Deck1.Count > 0 && Deck2.Count > 0)
+            {
+                bool PlayerOneWins = false;
+                int PlayerOneCard = Deck1.Dequeue();
+                int PlayerTwoCard = Deck2.Dequeue();
+                if (PlayerOneCard <= Deck1.Count && PlayerTwoCard <= Deck2.Count)
+                {
+                    PlayerOneWins = Crab(new Queue<int>(Deck1.Take(PlayerOneCard)), new Queue<int>(Deck2.Take(PlayerTwoCard)));
+                }
+                else //Not recursive
+                {
+                    PlayerOneWins = (PlayerOneCard > PlayerTwoCard);
+                }
+                if (PlayerOneWins)
+                {
+                    Deck1.Enqueue(PlayerOneCard);
+                    Deck1.Enqueue(PlayerTwoCard);
+                }
+                else
+                {
+                    Deck2.Enqueue(PlayerTwoCard);
+                    Deck2.Enqueue(PlayerOneCard);
+                }
+                string Round = string.Join(",", Deck1) + string.Join(",", Deck2);
+                if (AllTheRounds.Contains(Round))
+                {
+                    break;
+                }
+                else
+                    AllTheRounds.Add(Round);
+            }
+            return (Deck1.Count > 0);
         }
     }
 }
