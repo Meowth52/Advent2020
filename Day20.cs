2c1ba01d7e95d9fbc41a8c36f0d6691510a4f9f5
Meowth52 <meowth@meowth.se>
2021-11-23 20:10:36 +0100

    day 20 part 2 started. While(true) atm

--- Day20.cs
+++ Day20.cs
@@ -10,11 +10,13 @@ namespace Advent2020
     public class Day20 : Day
     {
         Dictionary<int, Square> Squares;
+        Dictionary<int, List<bool>> Corners;
         public Day20(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
             string[] splitted = Input.Split(new[] { "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries);
             Squares = new Dictionary<int, Square>();
+            Corners = new Dictionary<int, List<bool>>();
             foreach (string s in splitted)
             {
                 string[] Splittted = s.Split(new[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
@@ -36,6 +38,7 @@ namespace Advent2020
             foreach (KeyValuePair<int, Square> s in Squares)
             {
                 int NumberOfNeighbours = 0;
+                List<bool> Matches = new List<bool>() { false, false, false, false };
                 foreach (KeyValuePair<int, Square> s2 in Squares)
                 {
                     if (s.Key != s2.Key)
@@ -43,18 +46,79 @@ namespace Advent2020
                         foreach (int side in s.Value.Sides)
                         {
                             if (s2.Value.Sides.Contains(side) || s2.Value.AntiSides.Contains(side))
+                            {
                                 NumberOfNeighbours++;
+                                Matches[s.Value.Sides.IndexOf(side)] = true;
+                            }
                         }
                     }
                 }
                 if (NumberOfNeighbours == 2)
+                {
                     ReturnValue *= s.Key;
+                    Corners.Add(s.Key, Matches);
+                }
             }
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
             int ReturnValue = 0;
+            List<List<char>> Sea = new List<List<char>>();
+            int Current = Corners.First().Key;
+            List<int> Directions = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                if (Corners[Current][i])
+                    Directions.Add(i);
+            }
+            while (true)
+            {
+                int side = Squares[Current].Sides[Directions[0]];
+                //match first direction and fill the sea if no match go to second direction
+            }
+            foreach (KeyValuePair<int, Square> s in Squares)
+            {
+                foreach (KeyValuePair<int, Square> s2 in Squares)
+                {
+                    if (s.Key != s2.Key)
+                    {
+                        for (int i = 0; i < 4; i++)
+                        {
+                            int side = s.Value.Sides[i];
+                            if (s2.Value.Sides.Contains(side) || s2.Value.AntiSides.Contains(side))
+                            {
+                                switch (i)
+                                {
+                                    case 3://right
+                                        s.Value.MatchRight = s2.Key;
+                                        break;
+                                    case 1://down
+                                        s.Value.MatchDown = s2.Key;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                            if (s2.Value.AntiSides.Contains(side))
+                            {
+                                switch (i)
+                                {
+                                    case 3://right
+                                        s.Value.InvertRight = true;
+                                        break;
+                                    case 1://down
+                                        s.Value.InvertDown = true;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
             return ReturnValue.ToString();
         }
     }
@@ -62,6 +126,10 @@ namespace Advent2020
     {
         public List<int> Sides; //top,down,left,right
         public List<int> AntiSides;
+        public int MatchRight;
+        public bool InvertRight;
+        public int MatchDown;
+        public bool InvertDown;
         public char[,] Content;
         int Size;
         public Square(string input)
@@ -113,6 +181,22 @@ namespace Advent2020
                 }
             }
             Content = New;
+            List<int> NewSides = new List<int>()
+            {
+                AntiSides[2],
+                Sides[0],
+                AntiSides[3],
+                Sides[1]
+            }; //top,down,left,right
+            List<int> NewAntiSides = new List<int>()
+            {
+                Sides[2],
+                AntiSides[0],
+                Sides[3],
+                AntiSides[1]
+            };
+            Sides = new List<int>(NewSides);
+            AntiSides = new List<int>(NewAntiSides);
         }
         public void InvertX()
         {
@@ -125,6 +209,22 @@ namespace Advent2020
                 }
             }
             Content = New;
+            List<int> NewSides = new List<int>()
+            {
+                AntiSides[0],
+                AntiSides[1],
+                Sides[3],
+                Sides[2]
+            }; //top,down,left,right
+            List<int> NewAntiSides = new List<int>()
+            {
+                Sides[0],
+                Sides[1],
+                AntiSides[3],
+                AntiSides[2]
+            };
+            Sides = new List<int>(NewSides);
+            AntiSides = new List<int>(NewAntiSides);
         }
         public void InvertY()
         {
@@ -137,6 +237,22 @@ namespace Advent2020
                 }
             }
             Content = New;
+            List<int> NewSides = new List<int>()
+            {
+                Sides[1],
+                Sides[0],
+                AntiSides[2],
+                AntiSides[3]
+            }; //top,down,left,right
+            List<int> NewAntiSides = new List<int>()
+            {
+                AntiSides[1],
+                AntiSides[0],
+                Sides[2],
+                Sides[3]
+            };
+            Sides = new List<int>(NewSides);
+            AntiSides = new List<int>(NewAntiSides);
         }
     }
 }
